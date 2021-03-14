7db0799c069bcea6995a9c66be2ac4df23420413
machukhinktato <tarabrinmv@gmail.com>
2021-03-14 15:52:40 +0300

    in  progress

---
+++ students/Тарабрин Михаил/pw_5/script.js
@@ -0,0 +1,108 @@
+const API_ROOT = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+const request = (path = '', method = 'GET', body) => {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    console.log({response: xhr.responseText});
+                    resolve(JSON.parse(xhr.responseText));
+                } else {
+                    console.error(xhr.responseText);
+                    reject(xhr.responseText);
+                }
+            }
+        }
+
+        xhr.open(method, `${API_ROOT}/${path}`);
+
+        xhr.send(body);
+    });
+}
+
+new Vue({
+    el: '#app',
+    data: {
+        goods: [],
+        searchValue: '',
+        basketGoods: [],
+    },
+    created() {
+        this.fetchGoods();
+        this.fetchBasket();
+    },
+    computed: {
+        filteredGoods() {
+            const regexp = new RegExp(this.searchValue, 'i');
+            return this.goods.filter((goodsItem) =>
+                regexp.test(goodsItem.product_name)
+            );
+        },
+        basketTotal() {
+            return this.basketGoods.reduce(
+                (accumulator, currentElement) => accumulator + currentElement.price * currentElement.quantity,
+                0
+            );
+        }
+    },
+    methods: {
+        async fetchGoods() {
+            try {
+                const res = await fetch(`${API_ROOT}/catalogData.json`);
+                const goods = await res.json();
+                this.goods = goods;
+            } catch (error) {
+                console.log(`Can't fetch data`, error);
+                throw new Error(error);
+            }
+        },
+        isVisibleCart() {
+            let modal= document.querySelector('.modal');
+            let visible = modal.style.display;
+            modal.style.display = 'block';
+            if (visible === 'block') {
+                modal.style.display = 'none';
+            } else {
+                modal.style.display = 'block';
+            }
+        },
+        async fetchBasket() {
+            try {
+                const res = await fetch(`${API_ROOT}/getBasket.json`);
+                const goods = await res.json();
+                this.basketGoods = goods.contents;
+            } catch (error) {
+                console.log(`Can't fetch data`, error);
+                throw new Error(error);
+            }
+        },
+        addItem(item) {
+            request('addToBasket.json')
+                .then((response) => {
+                    if (response.result !== 0) {
+                        const itemIndex = this.basketGoods.findIndex((goodsItem) => goodsItem.id_product === item.id_product);
+                        if (itemIndex > -1) {
+                            this.basketGoods[itemIndex].quantity += 1;
+                        } else {
+                            this.basketGoods.push({...item, quantity: 1});
+                        }
+                        console.log(this.basketGoods);
+                    } else {
+                        console.error(`Can't add item to basket`, item, this.basketGoods);
+                    }
+                })
+        },
+        removeItem(id) {
+            request('deleteFromBasket.json')
+                .then((response) => {
+                    if (response.result !== 0) {
+                        this.basketGoods = this.basketGoods.filter((goodsItem) => goodsItem.id_product !== parseInt(id));
+                        console.log(this.basketGoods);
+                    } else {
+                        console.error(`Can't remove item from basket`, item, this.basketGoods);
+                    }
+                });
+        }
+    },
+});
