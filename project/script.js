f4d47c8130b40a83a75dc6c3ad8f5cc19b4cc111
Mikhail Tarabrin <51959562+machukhinktato@users.noreply.github.com>
2021-03-15 17:16:02 +0300

    revert
    не понял как через интерфейс github откатить коммит, случайно не туда, залил своё практическое задание

--- project/script.js
+++ project/script.js
@@ -2,11 +2,11 @@ const API_ROOT = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-st
 const request = (path = '', method = 'GET', body) => {
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
-
+        
         xhr.onreadystatechange = () => {
             if (xhr.readyState === 4) {
                 if (xhr.status === 200) {
-                    console.log({response: xhr.responseText});
+                    console.log({ response: xhr.responseText });
                     resolve(JSON.parse(xhr.responseText));
                 } else {
                     console.error(xhr.responseText);
@@ -14,9 +14,9 @@ const request = (path = '', method = 'GET', body) => {
                 }
             }
         }
-
+        
         xhr.open(method, `${API_ROOT}/${path}`);
-
+        
         xhr.send(body);
     });
 }
@@ -27,7 +27,6 @@ new Vue({
         goods: [],
         searchValue: '',
         basketGoods: [],
-        banana: "none",
     },
     created() {
         this.fetchGoods();
@@ -36,13 +35,13 @@ new Vue({
     computed: {
         filteredGoods() {
             const regexp = new RegExp(this.searchValue, 'i');
-            return this.goods.filter((goodsItem) =>
+            return this.goods.filter((goodsItem) => 
                 regexp.test(goodsItem.product_name)
             );
         },
-        basketTotal() {
-            return this.basketGoods.reduce(
-                (accumulator, currentElement) => accumulator + currentElement.price * currentElement.quantity,
+        total() {
+            return this.goods.reduce(
+                (accumulator, currentElement) => accumulator + currentElement.price,
                 0
             );
         }
@@ -53,63 +52,47 @@ new Vue({
                 const res = await fetch(`${API_ROOT}/catalogData.json`);
                 const goods = await res.json();
                 this.goods = goods;
-            } catch (error) {
+            } catch (err) {
                 console.log(`Can't fetch data`, error);
                 throw new Error(error);
             }
         },
-        // дублирует isVisibleCart, работает с внутр. аттрибутом
-        bananaChanger() {
-            if (this.banana == "display:none") {
-                this.banana = "display:block"
-            } else {
-                this.banana = "display:none"
-            }
+        fetchBasket() {
+            request('getBasket.json')
+                .then((goods) => {
+                    this.basketGoods = goods.contents;
+                    console.log('basket', this.basketGoods);
+                })
+                .catch((error) => {
+                    console.log(`Can't fetch basket data`, error);
+                });
         },
-        isVisibleCart() {
-            let modal = document.querySelector('.modal');
-            if (modal.style.display === 'block') {
-                modal.style.display = 'none';
-            } else {
-                modal.style.display = 'block';
-            }
-        },
-        async fetchBasket() {
-            try {
-                const res = await fetch(`${API_ROOT}/getBasket.json`);
-                const goods = await res.json();
-                this.basketGoods = goods.contents;
-            } catch (error) {
-                console.log(`Can't fetch data`, error);
-                throw new Error(error);
-            }
-        },
-        async addItem(item) {
-            try {
-                const res = await fetch(`${API_ROOT}/addToBasket.json`);
-                const response = await res.json();
+        addItem(item) {
+            request('addToBasket.json')
+                .then((response) => {
                     if (response.result !== 0) {
                         const itemIndex = this.basketGoods.findIndex((goodsItem) => goodsItem.id_product === item.id_product);
                         if (itemIndex > -1) {
                             this.basketGoods[itemIndex].quantity += 1;
                         } else {
-                            this.basketGoods.push({...item, quantity: 1});
-                        console.log(itemIndex)
-                        }}
-            } catch (err) {
-                console.error(`Can't add item to basket`, item, this.basketGoods, err);;
-            }
+                            this.basketGoods.push({ ...item, quantity: 1 });
+                        }
+                        console.log(this.basketGoods);
+                    } else {
+                        console.error(`Can't add item to basket`, item, this.basketGoods);
+                    }
+                })
         },
-        async removeItem(item) {
-            try {
-                const res = await fetch(`${API_ROOT}/deleteFromBasket.json`);
-                const response = await res.json();
+        removeItem(id) {
+            request('deleteFromBasket.json')
+                .then((response) => {
                     if (response.result !== 0) {
-                        this.basketGoods = this.basketGoods.filter((goodsItem) => goodsItem.id_product !== parseInt(item.id_product));
-            }
-        } catch(err) {
-            console.error(`Can't remove item from basket`, item, this.basketGoods, err);
+                        this.basketGoods = this.basketGoods.filter((goodsItem) => goodsItem.id_product !== parseInt(id));
+                        console.log(this.basketGoods);
+                    } else {
+                        console.error(`Can't remove item from basket`, item, this.basketGoods);
+                    }
+                });
         }
-        },
     },
 });
